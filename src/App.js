f1a675acd5201f4fe4e3cbb9da8f9c0205754c88
Jordan Roberts <jordanroberts@talk21.com>
2019-09-08 12:47:04 +0100

    Added API information from Edamam and stored in Environment Variables for safety. .env file is in .gitignore so should not upload to git.
    Included the example request from Edamam's documentation and saved as a constant.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+
+const App = () => {
+
+  const APP_ID = (process.env.REACT_APP_APP_ID)
+  const APP_KEY = (process.env.REACT_APP_APP_KEY); 
+  
+  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}" // from Edamam documentation
+ 
+  return(
+    
+    <div className="App">
+    <h1>Hello React</h1>
+    </div>
+    
+  )
 }
+
 export default App;
