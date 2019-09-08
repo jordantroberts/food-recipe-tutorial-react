830ff33b7a983249ce331ea16bec97675a301257
Jordan Roberts <jordanroberts@talk21.com>
2019-09-08 16:23:55 +0100

    Added module.css file for CSS that will only apply to recipes component

--- src/recipe.js
+++ src/recipe.js
@@ -1,16 +1,17 @@
 import React from "react";
+import style from './recipe.module.css';
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
       <p>{calories}</p>
       <img src={image} alt=""></img>
-      <ul>
+      <ol>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 };
